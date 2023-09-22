0e5a7c1540c5e6ddc77ba52677fa35b24ef414f1
ShivamBansal07 <sb2368@srmist.edu.in>
2023-09-22 23:14:36 +0530

    fix: made analytic Page dynamic

--- lib/screens/analytic_screen.dart
+++ lib/screens/analytic_screen.dart
@@ -3,16 +3,17 @@ import 'package:kzlinks/components/analytic_box.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AnalyticScreen extends StatelessWidget {
-  const AnalyticScreen({super.key});
+  final String analyticCode;
+  const AnalyticScreen({required this.analyticCode,super.key});
 
   @override
   Widget build(BuildContext context) {
+    //fetch data this data
     Map<String, List<String>> data = {
       'Operating Systems': ['Windows', 'Mac', 'Linux', 'Android', 'iOS'],
       'Browsers': ['Chrome', 'Firefox', 'Safari', 'Edge', 'Opera'],
       'locations': ['India', 'USA', 'UK', 'Canada', 'Australia'],
     };
-
     const clicks = '4100';
     const link = 'https://kzilla.xyz/';
     return Scaffold(
@@ -89,88 +90,86 @@ class AnalyticScreen extends StatelessWidget {
               thickness: 5,
               interactive: true,
               radius: const Radius.circular(10),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    //link Widget
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: SizedBox(
-                        height: 65,
-                        child: Container(
-                            height: 50,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
+              child: Column(
+                children: [
+                  //link Widget
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: SizedBox(
+                      height: 65,
+                      child: Container(
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            color: const Color(0xffF5F5F5),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: const Center(
+                            child: Text(
+                              link,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                              ),
+                            ),
+                          )),
+                    ),
+                  ),
+                  //Analytics Widget List
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(12),
-                              color: const Color(0xffF5F5F5),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.3),
-                                  spreadRadius: 2,
-                                  blurRadius: 5,
-                                  offset: const Offset(0, 3),
+                              color: Colors.black),
+                          child: const Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  'Clicks',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  clicks,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
-                            child: const Center(
-                              child: Text(
-                                link,
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 17,
-                                ),
-                              ),
-                            )),
-                      ),
+                          )),
                     ),
-                    //Analytics Widget List
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Container(
-                            height: 50,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                color: Colors.black),
-                            child: const Center(
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Text(
-                                    'Clicks',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 17,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    clicks,
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 17,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                            )),
-                      ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: ScrollableListViewWidget(
+                                data.keys.toList()[index],
+                                data.values.toList()[index]));
+                      },
                     ),
-                    // Expanded(child:
-                    // ListView.builder(
-                    //   itemCount: 1,
-                    //   itemBuilder: (context, index) {
-                    //     return const Text('Hello World');
-                    //   },
-                    // ),),
-                    Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: ScrollableListViewWidget(data.keys.first,data.values.first),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
